92743ebc03bfc7825597acc4de2a2c01c27c11b8
saiakhil <saiakhilpandiri@gmail.com>
2021-12-18 09:36:55 +0530

    printing data

---
+++ 03-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## if we assign a name to a set of data is called as variable
+
+# syntax: VARNAME=DATA
+a=100     # number
+b=apple   # string
+c=true    # boolean
+
+# shell does not have any data type by default
+
